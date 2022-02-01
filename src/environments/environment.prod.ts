c550e2f3cd0ca240d6a39f9d8225bb84998b68d5
ruandeveloper <ruanoliveira862@gmail.com>
2022-01-31 23:06:15 -0300

    chore: updt token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: false,
 	defaultTheme: 'dark',
 	githubGraphQlUrl: 'https://api.github.com/graphql',
-	githubToken: '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'
+	githubToken: ''
 };
